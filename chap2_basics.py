06b7ed9ffcfc858bb2ab3ed5c51b5faedd6b4413
agbleze <lagbleze@st.ug.edu.gh>
2023-04-18 23:56:53 +0200

    Update chap2_basics.py

--- chap2_basics.py
+++ chap2_basics.py
@@ -11,11 +11,20 @@ spark = SparkSession.builder.appName("Data Wrangling").getOrCreate()
 #%%
 data_link ="https://raw.githubusercontent.com/Apress/applied-data-science-using-pyspark/main/Ch02/Chapter2_Data/movie_data_part1.csv"
 
+#%%
+pd.read_csv(data_link, on_bad_lines="skip")
 
-pd.read_csv(data_link, header=None)
+
+#%%
+
+import requests
 
 
+url = "https://raw.githubusercontent.com/Apress/applied-data-science-using-pyspark/main/Ch02/Chapter2_Data/movie_data_part1.csv"
 
+r = requests.get(url, allow_redirects=True)
+
+open('movie_data.csv', 'wb').write(r.content)
 
 
 
