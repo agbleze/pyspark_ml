dd2d52648916e512b807d5952d8fe17c07b65936
agbleze <lagbleze@st.ug.edu.gh>
2023-04-26 14:41:20 +0200

    Update chap5_ml.py

---
+++ chap5_ml.py
@@ -0,0 +1,45 @@
+
+#%%
+
+filename = "bank-full.csv"
+
+from pyspark.sql import SparkSession
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml import Pipeline
+
+#%%
+spark = SparkSession.builder.getOrCreate()
+data = spark.read.csv(filename, header=True, inferSchema=True, 
+                      sep=';')
+
+data.show()
+
+# assemble feature vectors
+
+def AssembleVectors(df, features_list, target_variable_name):
+    assembler = VectorAssembler(inputCols=features_list,
+                                outputCol='features'
+                                )
+    stages = [assembler]
+    
+    selectedCols = [target_variable_name, 'features'] + features_list
+    
+    # use pipeline to process sequentially
+    pipeline = Pipeline(stages=stages)
+    assembleModel = pipeline.fit(df)
+    
+    # apply assembler model on data
+    df = assembleModel.transform(df).select(selectedCols)
+    return df
+
+
+
+
+
+
+
+
+
+
+
+
